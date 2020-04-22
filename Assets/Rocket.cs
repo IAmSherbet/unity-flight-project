db25c7527910c5103b1a62211442c701c5aefe84
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-04-22 22:40:42 +1000

    43 & 44: Bound thrust, left and right keys to the Rocket, and now prints to console when pressed.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>(); // Unity provided function. Gets all components of type RigidBody within context
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up); // Position of the ship is a vector three, three floating point numbers
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
+}
