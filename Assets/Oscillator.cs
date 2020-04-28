9ab4924213a5b2763cbfb22d467827fd03643f84
Sherwesh Bajracharya <sbajracharya@fortuitycreative.com>
2020-04-29 08:07:17 +1000

    62. Made the platform oscillate automatically!

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -4,11 +4,13 @@
 
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
-    Vector3 startingPos;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
     // todo: remove from inspector later
     [Range(0,1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved
+
+    Vector3 startingPos;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,14 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // todo: protect against period = 0, which would result in 0/0 NotANumber error
+        float cycles = Time.time / period; // grows continually from zero
+
+        const float tau = Mathf.PI * 2f; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // generates number from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
